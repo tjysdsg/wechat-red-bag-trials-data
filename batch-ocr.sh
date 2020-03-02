407295b86b7a92ffb38535e100d5ad7fd128e81b
tjysdsg <tjy1018543509@163.com>
2020-03-02 23:02:32 +0800

    initial commit

---
+++ batch-ocr.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+for f in `find ./ -type f \( -iname \*.jpg -o -iname \*.png \)`; do
+    filename=$(basename -- "$f")
+    filename="${filename%.*}"
+    echo "OCRing $f"
+    tesseract $f $filename -l chi_sim+eng --psm 6
+done
