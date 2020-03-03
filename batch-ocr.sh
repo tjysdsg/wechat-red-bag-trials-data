adeafdfb0786a7f5827babda4cac00260a2b21c8
tjysdsg <tjy1018543509@163.com>
2020-03-03 21:17:51 +0800

    Add 101 - 200 trials

--- batch-ocr.sh
+++ batch-ocr.sh
@@ -4,5 +4,5 @@ for f in `find ./ -type f \( -iname \*.jpg -o -iname \*.png \)`; do
     filename=$(basename -- "$f")
     filename="${filename%.*}"
     echo "OCRing $f"
-    tesseract $f $filename -l chi_sim+eng --psm 6
+    tesseract $f $filename -l chi_sim+eng
 done
